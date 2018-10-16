3719c82f3a58ee4f27b6a129dbeab96144bd5fe2
usernameisalreadytakenwasalreadytaken <robert.brown-9@student.manchester.ac.uk>
2018-10-16 16:15:10 +0100

    Completed NeutronAttenuationSpherical run, & python file to generate g4bl files

--- g4beamline files/NeutronAttenuationSpherical/createg4bl.py
+++ g4beamline files/NeutronAttenuationSpherical/createg4bl.py
@@ -1,11 +1,16 @@
 from numpy import arange
 import os
 
+sphere_radius = 500
+
 for detnum in arange(10,300,10):
     with open("template.txt", 'r') as templatef:
         os.makedirs(os.getcwd() + "/Det{num}/".format(num=detnum))
         with open(os.getcwd() + "/Det{num}/det.g4bl".format(num=detnum), 'w+') as f:
             for line in templatef:
-                if line.startswith("sphere Target"):
-                    line = line.replace("$outerRadius$",str(detnum))
+                line = line.replace("$detNum$",str(detnum))
+                line = line.replace("$outerRadius$",str(sphere_radius + detnum))
+                line = line.replace("$innerRadius$",str(sphere_radius))
                 print(line, file=f, end='')
+
+exit()
--- g4beamline files/NeutronAttenuationSpherical/rung4bl.sh
+++ g4beamline files/NeutronAttenuationSpherical/rung4bl.sh
@@ -1,7 +1,9 @@
 export G4_DIR=$HOME/G4beamline-3.04/bin
 
-# "$G4_DIR/g4bl" "$PWD/
+export CURR=$PWD
+cd ../../data/NeutronAttenuationSpherical/
+
 for i in `seq 10 30 300`
 do
-  "$G4_DIR/g4bl" "$PWD/Det$i/det.g4bl" & "$G4_DIR/g4bl" "$PWD/Det$(($i + 10))/det.g4bl" &  "$G4_DIR/g4bl" "$PWD/Det$(($i + 20))/det.g4bl"
+  "$G4_DIR/g4bl" "$CURR/Det$i/det.g4bl" & "$G4_DIR/g4bl" "$CURR/Det$(($i + 10))/det.g4bl" &  "$G4_DIR/g4bl" "$CURR/Det$(($i + 20))/det.g4bl"
 done
