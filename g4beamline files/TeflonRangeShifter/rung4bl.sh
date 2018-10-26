06a22f09b9f91bfbb8956d2b2a4fd66f078f8b61
usernameisalreadytakenwasalreadytaken <robert.brown-9@student.manchester.ac.uk>
2018-10-26 23:09:53 +0100

    Teflon Range Shifter data, Geant4 dose calculation for simple 1L cube of water,

--- g4beamline files/TeflonRangeShifter/rung4bl.sh
+++ g4beamline files/TeflonRangeShifter/rung4bl.sh
@@ -1,3 +1,5 @@
 # runs LeadNeutronMultiplicity with multithreading
 export G4_DIR=$HOME/G4beamline-3.04/bin
-"$G4_DIR/g4bl" "$PWD/1cm.g4bl"
+export CURR=$PWD
+cd ../../data/TeflonRangeShifter
+"$G4_DIR/g4bl" "$CURR/1cm.g4bl" & "$G4_DIR/g4bl" "$CURR/2cm.g4bl" & "$G4_DIR/g4bl" "$CURR/3cm.g4bl"
