837fafc9a0b81b06acf5c221296e2148dc54e478
usernameisalreadytakenwasalreadytaken <robert.brown-9@student.manchester.ac.uk>
2018-12-15 17:07:17 +0000

    ActivationPlot.py now takes the input json as an argument.

--- python files/ActivationPlot/ActivationPlot.py
+++ python files/ActivationPlot/ActivationPlot.py
@@ -36,75 +36,89 @@ def saturation(n, error, lifeTime, beam_time=60):
 actual_protons = 1e11
 isotopes_we_care_about = ["Be7", "C10", "C11", "O15", "O14", "N13", "N16", "F17", "F18", "Be11", "N17", "C15"]
 
-# Load data
-with open("rangeshifter_act.json") as f:
-    data = json.load(f)
+def make_plot(fileName, title, outName):
+    # Load data
+    with open(fileName) as f:
+        data = json.load(f)
 
-# Uncomment to care about all isotopes
-"""
-for run in data:
-    for ist in run["isotopes"].keys():
-        if ist not in isotopes_we_care_about:
-            isotopes_we_care_about.append(ist)
-"""
+    # Uncomment to care about all isotopes
+    """
+    for run in data:
+        for ist in run["isotopes"].keys():
+            if ist not in isotopes_we_care_about:
+                isotopes_we_care_about.append(ist)
+    """
 
-energy = []
-act = collections.defaultdict(list)
-act_error = collections.defaultdict(list)
+    energy = []
+    act = collections.defaultdict(list)
+    act_error = collections.defaultdict(list)
 
-for run in data:
-    energy.append(1e3 * run["energy"])
+    for run in data:
+        energy.append(1e3 * run["energy"])
+        for ist in isotopes_we_care_about:
+            try:
+                # Get number produced in run
+                n = run["isotopes"][ist]["number"]
+
+                # Normalise to 10^11 protons
+                normalised_n = n * actual_protons / run["nEvents"]
+
+                # Error in normalized N
+                normalised_error = np.sqrt(n) * actual_protons / run["nEvents"]
+
+                # Get saturation activity & error
+                r_act, r_act_error = saturation(
+                    n=normalised_n,
+                    error=normalised_error,
+                    lifeTime=run["isotopes"][ist]["lifeTime"]
+                )
+            except KeyError:
+                # None of this isotope created during this run, don't plot
+                r_act = float("nan");
+                r_act_error = float("nan");
+
+            # Append to lists
+            act[ist].append(r_act)
+            act_error[ist].append(r_act_error)
+
+    fig, ax = plt.subplots(figsize=(10.5, 14.85))  # Make figure half a4 size
+
+    # Make y axis logarithmic
+    ax.set_yscale("log", nonposy='clip', )
+
+    # Set horizontal ticks to 10 MeV
+    ax.set_xticks(np.arange(70,260,10))
+
+    # Set title and axis labels
+    ax.set_title(title)
+    ax.set_ylabel("Induced activity per 100 Giga protons (Bq)")
+    ax.set_xlabel("Energy (MeV)")
+
+    # Set limits
+    ax.set_xlim((70,250))
+    ax.set_ylim(ymin=1, ymax=2e6)
+
     for ist in isotopes_we_care_about:
-        try:
-            # Get number produced in run
-            n = run["isotopes"][ist]["number"]
+        plt.plot(energy, act[ist], label=format_isotope(ist))
+        plt.errorbar(energy, act[ist], yerr=act_error[ist], fmt=' o ', c='k', capsize=2, markersize=1)
 
-            # Normalise to 10^11 protons
-            normalised_n = n * actual_protons / run["nEvents"]
+    # Place legend in best place in bottom right quadrant
+    ax.grid(which='both', linewidth=0.7)
+    ax.grid(which='major', axis='y', linewidth=0.7, c='k')
 
-            # Error in normalized N
-            normalised_error = np.sqrt(n) * actual_protons / run["nEvents"]
+    plt.legend(bbox_to_anchor=(0.5, 0., 0.5, 0.5))
+    plt.savefig(outName)
 
-            # Get saturation activity & error
-            r_act, r_act_error = saturation(
-                n=normalised_n,
-                error=normalised_error,
-                lifeTime=run["isotopes"][ist]["lifeTime"]
-            )
-        except KeyError:
-            # None of this isotope created during this run, don't plot
-            r_act = float("nan");
-            r_act_error = float("nan");
 
-        # Append to lists
-        act[ist].append(r_act)
-        act_error[ist].append(r_act_error)
+# Main
+make_plot(
+    fileName="5cm-rangeshifter.json"
+    title="Activation of 5cm thick range shifter immediately after beam turned off"
+    outName="5cm-rangeshifter.svg"
+)
 
-fig, ax = plt.subplots(figsize=(10.5, 14.85))  # Make figure half a4 size
-
-# Make y axis logarithmic
-ax.set_yscale("log", nonposy='clip', )
-
-# Set horizontal ticks to 10 MeV
-ax.set_xticks(np.arange(70,260,10))
-
-# Set title and axis labels
-ax.set_title("Activation of 5cm thick range shifter immediately after beam turned off")
-ax.set_ylabel("Induced activity per 100 Giga protons (Bq)")
-ax.set_xlabel("Energy (MeV)")
-
-# Set limits
-ax.set_xlim((70,250))
-ax.set_ylim(ymin=1, ymax=2e6)
-
-for ist in isotopes_we_care_about:
-    plt.plot(energy, act[ist], label=format_isotope(ist))
-    plt.errorbar(energy, act[ist], yerr=act_error[ist], fmt=' o ', c='k', capsize=2, markersize=1)
-
-# Place legend in best place in bottom right quadrant
-ax.grid(which='both', linewidth=0.7)
-ax.grid(which='major', axis='y', linewidth=0.7, c='k')
-
-plt.legend(bbox_to_anchor=(0.5, 0., 0.5, 0.5))
-# plt.savefig("plot.pdf")
-plt.savefig("plot.svg")
+make_plot(
+    fileName="water.json"
+    title="Activation of water phantom immediately after beam turned off"
+    outName="water.svg"
+)
