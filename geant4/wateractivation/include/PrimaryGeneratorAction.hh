585c0be8007900042de1e9ca460801b410e4058e
usernameisalreadytakenwasalreadytaken <robert.brown-9@student.manchester.ac.uk>
2018-12-15 04:30:31 +0000

    comments

--- geant4/wateractivation/include/PrimaryGeneratorAction.hh
+++ geant4/wateractivation/include/PrimaryGeneratorAction.hh
@@ -3,8 +3,6 @@
 
 #include <G4VUserPrimaryGeneratorAction.hh>
 
-class G4ParticleGun;
-// Task 2b.1 Include the proper header file or forward-declare the class for GPS
 class G4GeneralParticleSource;
 
 class PrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction
@@ -15,9 +13,6 @@ public:
     void GeneratePrimaries(G4Event* anEvent) override;
 
 private:
-	// Task 2b.1: Replace the gun with a GPS instance called fGPS;
-    // G4ParticleGun* fGun;
-
     G4GeneralParticleSource* fGPS;
 };
 
