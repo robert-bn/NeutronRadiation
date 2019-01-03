36e749ae528ed4fd27a1a8a0578381ed10bd9a4f
Robert Brown <mbcx4rba@e-c07srm024.it.manchester.ac.uk>
2019-01-03 16:28:37 +0000

    removed out of range protons from DownrangeEnergyPloy.py

--- python files/DownrangeEnergyPlot/DownrangeSurvialPlot.py
+++ python files/DownrangeEnergyPlot/DownrangeSurvialPlot.py
@@ -6,7 +6,6 @@ df = pd.read_csv("DownrangeEnergySpectra.csv")
 
 thicknesses = [1,2,3,5]
 
-df = df[df["Ratio"] > 0.1]
 df["Kinetic Energy"]  *= 1e3
 df["Downstream Energy Mean"] *= 1e3
 df["Downstream Energy Std"] *= 1e3
@@ -15,21 +14,14 @@ fig, ax = plt.subplots()
 
 for t in thicknesses:
     df_t = df[df["Thickness"]==t]
-    df_t.plot(x="Kinetic Energy", y="Downstream Energy Mean", ax=ax)
-    ax.fill_between(
-        df_t["Kinetic Energy"],
-        df_t["Downstream Energy Mean"] - df_t["Downstream Energy Std"],
-        df_t["Downstream Energy Mean"] + df_t["Downstream Energy Std"],
-        alpha=0.5
-    )
-ax.set_xticks(np.arange(70,250,10), minor=True)
-ax.set_yticks(np.arange(0,250,10), minor=True)
+    df_t.plot(x="Kinetic Energy", y="Ratio", ax=ax)
+
 ax.grid(True, which="major")
 ax.grid(True, which="minor", alpha=0.4)
 ax.set_xlim([70,250])
-ax.set_ylim(bottom=0, top=250)
+ax.set_ylim([0.91,1])
 ax.legend(["{} cm range shifter".format(t) for t in thicknesses])
 
 plt.xlabel("Input mean kinetic energy of protons (MeV)")
-plt.ylabel("Output mean kinetic energy of protons (MeV)")
+plt.ylabel("Downstream survial fraction")
 plt.show()
